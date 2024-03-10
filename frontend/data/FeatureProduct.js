1c84450ad8d8d45a13db7360b2db9223b752bd55
akram-tello <a.tello@graduate.utm.my>
2024-03-11 01:17:22 +0800

    typo

--- frontend/data/FeatureProduct.js
+++ frontend/data/FeatureProduct.js
@@ -1,22 +1,22 @@
 export const FeatureProductData = [
   {
     id: 1,
-    imageSrc: "/assets/img/FeatureProduct/1.jpg",
+    imageSrc: "/assets/img/featureProduct/1.jpg",
     projectTitle: "Sleep app doesn’t just keep track of your time asleep.",
   },
   {
     id: 2,
-    imageSrc: "/assets/img/FeatureProduct/2.jpg",
+    imageSrc: "/assets/img/featureProduct/2.jpg",
     projectTitle: "Log your state of mind. ",
   },
   {
     id: 3,
-    imageSrc: "/assets/img/FeatureProduct/3.png",
+    imageSrc: "/assets/img/featureProduct/3.png",
     projectTitle: "Any case. Any strap. Any style you want.",
   },
   {
     id: 4,
-    imageSrc: "/assets/img/FeatureProduct/4.png",   
+    imageSrc: "/assets/img/featureProduct/4.png",   
     projectTitle: "LifeSpan can call for help when you need it.",
   },
   
